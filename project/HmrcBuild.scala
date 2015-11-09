87f73747ab867ccdf1007dcabaad5ebae42e6e84
Charles Kubicek <charles.kubicek@digital.hmrc.gov.uk>
2015-11-09 16:12:37 +0000

    updating reative-mongo libraries. Replaced play-reactivemongo dependency with simple-reactivemongo as play-reactivemongo was not necessary. Made play and mongo dependecies provided as the projet using this library should decide what versions to use

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -30,8 +30,8 @@ private object AppDependencies {
 
   val compile = Seq(
     filters,
-    "com.typesafe.play" %% "play" % PlayVersion.current,
-    "uk.gov.hmrc" %% "play-reactivemongo" % "4.3.0",
+    "com.typesafe.play" %% "play" % PlayVersion.current % "provided",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "4.2.1" % "provided",
     "uk.gov.hmrc" %% "time" % "2.0.0"
   )
 
