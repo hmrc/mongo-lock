b0c504fb59bb2cb4181d9fc960cbf1856508edb9
Konrad Wudkowski <wudkowski@gmail.com>
2018-11-21 16:19:20 +0000

    PLATOPS-1783: Downgrade reactivemongo to 0.15.0 due to a regression but in 0.15.1

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -5,21 +5,21 @@ object LibDependencies {
   def apply(): Seq[ModuleID] = compile ++ test
 
   private val play25Version = "2.5.12"
-  private val play26Version = "2.6.15"
+  private val play26Version = "2.6.20"
 
   private val compile: Seq[ModuleID] = PlayCrossCompilation.dependencies(
     shared = Seq(
-      "uk.gov.hmrc" %% "time" % "3.0.0"
+      "uk.gov.hmrc" %% "time" % "3.2.0"
     ),
     play25 = Seq(
       "com.typesafe.play" %% "filters-helpers"      % play25Version,
       "com.typesafe.play" %% "play"                 % play25Version,
-      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.0.0-play-25"
+      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.4.0-play-25"
     ),
     play26 = Seq(
       "com.typesafe.play" %% "filters-helpers"      % play26Version,
       "com.typesafe.play" %% "play"                 % play26Version,
-      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.0.0-play-26"
+      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.4.0-play-26"
     )
   )
 
@@ -29,11 +29,11 @@ object LibDependencies {
       "org.pegdown"   % "pegdown"    % "1.6.0" % Test
     ),
     play25 = Seq(
-      "uk.gov.hmrc"    %% "reactivemongo-test" % "4.1.0-play-25" % Test,
+      "uk.gov.hmrc"    %% "reactivemongo-test" % "4.2.0-play-25" % Test,
       "ch.qos.logback" % "logback-classic"     % "1.1.2"         % Test
     ),
     play26 = Seq(
-      "uk.gov.hmrc"    %% "reactivemongo-test" % "4.1.0-play-26" % Test,
+      "uk.gov.hmrc"    %% "reactivemongo-test" % "4.2.0-play-26" % Test,
       "ch.qos.logback" % "logback-classic"     % "1.2.3"         % Test
     )
   )
