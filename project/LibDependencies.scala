f14a6cc0461e16413f7c1d1639fc0e2d712c809b
Konrad Wudkowski <11072914+konradwudkowski@users.noreply.github.com>
2019-01-22 16:41:18 +0000

    PLATOPS-1902: Upgrade ReactiveMongo to 0.16.1
    This fixes the performance problem with reconnecting present
    in 0.15.1 and 0.16.0.

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -4,7 +4,7 @@ object LibDependencies {
   
   def apply(): Seq[ModuleID] = compile ++ test
 
-  private val play25Version = "2.5.12"
+  private val play25Version = "2.5.19"
   private val play26Version = "2.6.20"
 
   private val compile: Seq[ModuleID] = PlayCrossCompilation.dependencies(
@@ -14,27 +14,26 @@ object LibDependencies {
     play25 = Seq(
       "com.typesafe.play" %% "filters-helpers"      % play25Version,
       "com.typesafe.play" %% "play"                 % play25Version,
-      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.6.0-play-25"
+      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.9.0-play-25"
     ),
     play26 = Seq(
       "com.typesafe.play" %% "filters-helpers"      % play26Version,
       "com.typesafe.play" %% "play"                 % play26Version,
-      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.6.0-play-26"
+      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.9.0-play-26"
     )
   )
 
   private val test: Seq[ModuleID] = PlayCrossCompilation.dependencies(
     shared = Seq(
-      "org.scalatest" %% "scalatest" % "3.0.5" % Test,
-      "org.pegdown"   % "pegdown"    % "1.6.0" % Test
+      "org.scalatest"  %% "scalatest"       % "3.0.5" % Test,
+      "org.pegdown"    %  "pegdown"         % "1.6.0" % Test,
+      "ch.qos.logback" %  "logback-classic" % "1.2.3" % Test
     ),
     play25 = Seq(
-      "uk.gov.hmrc"    %% "reactivemongo-test" % "4.4.0-play-25" % Test,
-      "ch.qos.logback" % "logback-classic"     % "1.1.2"         % Test
+      "uk.gov.hmrc"    %% "reactivemongo-test" % "4.6.0-play-25" % Test
     ),
     play26 = Seq(
-      "uk.gov.hmrc"    %% "reactivemongo-test" % "4.4.0-play-26" % Test,
-      "ch.qos.logback" % "logback-classic"     % "1.2.3"         % Test
+      "uk.gov.hmrc"    %% "reactivemongo-test" % "4.6.0-play-26" % Test
     )
   )
 }
