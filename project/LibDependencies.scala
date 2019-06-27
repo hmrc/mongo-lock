f635d366231c4942cee7bc88fd54ea3e31b7f34b
Christopher Turner <23338096+christopherjturner@users.noreply.github.com>
2019-06-27 09:26:57 +0100

    BDOG-234 update simple-reactive-mongo dependency

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -5,7 +5,7 @@ object LibDependencies {
   def apply(): Seq[ModuleID] = compile ++ test
 
   private val play25Version = "2.5.19"
-  private val play26Version = "2.6.20"
+  private val play26Version = "2.6.23"
 
   private val compile: Seq[ModuleID] = PlayCrossCompilation.dependencies(
     shared = Seq(
@@ -20,7 +20,7 @@ object LibDependencies {
     play25 = Seq(
       "com.typesafe.play" %% "filters-helpers"      % play25Version,
       "com.typesafe.play" %% "play"                 % play25Version,
-      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.14.0-play-25",
+      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.20.0-play-25",
       // force dependencies due to security flaws found in xercesImpl 2.11.0
       // only applies to play 2.5 since it was removed from play 2.6 
       // https://github.com/playframework/playframework/blob/master/documentation/manual/releases/release26/migration26/Migration26.md#xercesimpl-removal
@@ -29,9 +29,8 @@ object LibDependencies {
     play26 = Seq(
       "com.typesafe.play" %% "filters-helpers"      % play26Version,
       "com.typesafe.play" %% "play"                 % play26Version,
-      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.14.0-play-26"
-    )
-  )
+      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.20.0-play-26"
+  ))
 
   private val test: Seq[ModuleID] = PlayCrossCompilation.dependencies(
     shared = Seq(
@@ -40,10 +39,10 @@ object LibDependencies {
       "ch.qos.logback" %  "logback-classic" % "1.2.3" % Test
     ),
     play25 = Seq(
-      "uk.gov.hmrc"    %% "reactivemongo-test" % "4.9.0-play-25" % Test
+      "uk.gov.hmrc"    %% "reactivemongo-test" % "4.15.0-play-25" % Test
     ),
     play26 = Seq(
-      "uk.gov.hmrc"    %% "reactivemongo-test" % "4.9.0-play-26" % Test
+      "uk.gov.hmrc"    %% "reactivemongo-test" % "4.15.0-play-26" % Test
     )
   )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,10 +3,10 @@ resolvers += Resolver.url("hmrc-sbt-plugin-releases", url("https://dl.bintray.co
 
 resolvers += Resolver.bintrayRepo("hmrc", "releases")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.13.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.16.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "1.15.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "1.19.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-artifactory" % "0.17.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-artifactory" % "0.19.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-play-cross-compilation" % "0.15.0")
