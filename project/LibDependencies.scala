b233d282956393a68decf3605c02ef26f4a99c92
Alex Olkhovskiy <lxol@users.noreply.github.com>
2020-01-08 11:28:02 +0000

    [DC-2186] update simple-reactivemongo

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -20,7 +20,7 @@ object LibDependencies {
     play25 = Seq(
       "com.typesafe.play" %% "filters-helpers"      % play25Version,
       "com.typesafe.play" %% "play"                 % play25Version,
-      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.20.0-play-25",
+      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.22.0-play-25",
       // force dependencies due to security flaws found in xercesImpl 2.11.0
       // only applies to play 2.5 since it was removed from play 2.6 
       // https://github.com/playframework/playframework/blob/master/documentation/manual/releases/release26/migration26/Migration26.md#xercesimpl-removal
@@ -29,7 +29,7 @@ object LibDependencies {
     play26 = Seq(
       "com.typesafe.play" %% "filters-helpers"      % play26Version,
       "com.typesafe.play" %% "play"                 % play26Version,
-      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.20.0-play-26"
+      "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.22.0-play-26"
   ))
 
   private val test: Seq[ModuleID] = PlayCrossCompilation.dependencies(
